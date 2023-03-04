12e6d6fbdc34e15b1b075bbaae6c9375b3b4ed44
teksediqa <sediqa.mobariz@tekschool.us>
2023-03-04 21:25:33 +0000

    this is the pull request to review the code

--- src/testgit/GitPracticeTwo.java
+++ src/testgit/GitPracticeTwo.java
@@ -1,15 +1,12 @@
 package testgit;
 
 public class GitPracticeTwo { 
-	
-	
-	
-	
-
 	public static void main(String[] args) {
 		int a  = 12; 
 		int b = 2; 
-		System.out.println(a*b);
+		System.out.println(a*b); 
+		
+		System.out.println("this is for pull request");
 
 	} 
 	
